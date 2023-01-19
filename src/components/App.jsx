f6f97d05319a25b5a57e9b015f46b332d5e921fa
Natalya Karpova <knataly0513@gmail.com>
2023-01-19 15:37:19 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from "./Searchbar";
 import { Box } from "./Box";
 
-export class App extends Component {
+export const App = () => {
+  const [searchValue, setSearchValue] = useState('');
 
-  state = {
-    searchValue: '',
-  }
-
-  handleFormSubmit = searchValue => {
-    this.setState({ searchValue });
+  const handleFormSubmit = searchValue => {
+    setSearchValue(searchValue);
   }
   
-  render() {
-  
-    return (
-      <Box
-        display="grid"
-        gridTemplateColumns="1fr"
-        gridGap="16px"
-        pb="24px"
-      >
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ToastContainer autoClose={2000} position="top-right" theme="light" />
-        <ImageGallery searchValue={this.state.searchValue}/>
-      </Box>
-    );
-  };
+  return (
+    <Box
+      display="grid"
+      gridTemplateColumns="1fr"
+      gridGap="16px"
+      pb="24px"
+    >
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ToastContainer autoClose={2000} position="top-right" theme="light" />
+      <ImageGallery searchValue={searchValue} />
+    </Box>
+  );
 };
