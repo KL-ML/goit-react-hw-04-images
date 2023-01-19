7640c4410269aa300323528945d6f72a7dad4ead
Natalya Karpova <knataly0513@gmail.com>
2023-01-19 15:37:22 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,9 +1,6 @@
-// import React, { PureComponent } from "react";
 import React from 'react';
-// import PropTypes from 'prop-types';
 import { GalleryItemImage } from './ImageGalleryItem.styled';
 
-// export class ImageGalleryItem extends PureComponent {
 export const ImageGalleryItem = ({ largeImageURL, webformatURL, tags, id }) => {
     
     return (
