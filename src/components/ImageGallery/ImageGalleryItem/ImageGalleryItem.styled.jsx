0967280f8a9b4d10b09ae24fc99ad94f3e227829
Natalya Karpova <knataly0513@gmail.com>
2023-01-20 11:55:57 +0200

    Update ImageGalleryItem.styled.jsx

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.styled.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.styled.jsx
@@ -9,6 +9,7 @@ export const GalleryItemImage = styled.img`
     width: 100%;
     height: 260px;
     object-fit: cover;
+    border-radius: 3px;
     transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
     &:hover {
         transform: scale(1.03);
